f5f5c7f9fb5720b2c3a1099d7183db21da0e8957
Jonah Unuafe <fejisfejiro@gmail.com>
2024-12-31 10:55:57 +0100

    add images and text to sign in page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,25 @@
-import { SplashScreen, Stack } from "expo-router";
-import "./globals.css";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { SplashScreen, Stack } from 'expo-router';
+import './globals.css';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-  const [ fontsLoaded ] = useFonts({
-    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
-    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
-    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
-    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
-    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf")
-  })
+  const [fontsLoaded] = useFonts({
+    'Rubik-Bold': require('../assets/fonts/Rubik-Bold.ttf'),
+    'Rubik-ExtraBold': require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    'Rubik-Light': require('../assets/fonts/Rubik-Light.ttf'),
+    'Rubik-Medium': require('../assets/fonts/Rubik-Medium.ttf'),
+    'Rubik-Regular': require('../assets/fonts/Rubik-Regular.ttf'),
+    'Rubik-SemiBold': require('../assets/fonts/Rubik-SemiBold.ttf'),
+  });
 
   useEffect(() => {
-    if(fontsLoaded) {
-      SplashScreen.hideAsync()
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
     }
-  }, [fontsLoaded])
+  }, [fontsLoaded]);
 
-  if(!fontsLoaded) return null;
+  if (!fontsLoaded) return null;
 
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
 
-export default SignIn
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to RealEstate
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}
+            <Text>Your Ideal Home</Text>
+          </Text>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
