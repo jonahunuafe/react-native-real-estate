222fa15f0794df003191fc6fee969624f2b49662
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-01 11:00:31 +0100

    set up appwrite config

---
+++ lib/appwrite.ts
@@ -0,0 +1,17 @@
+import { Account, Avatars, Client } from "react-native-appwrite";
+
+export const config = {
+  platform: "com.jonahDev.realestate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform)
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
