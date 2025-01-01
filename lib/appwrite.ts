d05ce1d936823a49b3028683a287f422f8aa8041
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-01 11:27:48 +0100

    add functionality to logout a user

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,52 +1,64 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
-import * as Linking from "expo-linking";
+import { Account, Avatars, Client, OAuthProvider } from 'react-native-appwrite';
+import * as Linking from 'expo-linking';
 
 export const config = {
-  platform: "com.jonahDev.realestate",
+  platform: 'com.jonahDev.realestate',
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
-}
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
 
 export const client = new Client();
 
 client
   .setEndpoint(config.endpoint!)
   .setProject(config.projectId!)
-  .setPlatform(config.platform)
+  .setPlatform(config.platform);
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
 
 export async function Login() {
   try {
-    const redirectUri = Linking.createURL("/");
+    const redirectUri = Linking.createURL('/');
 
-    const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
 
-    if(!response) throw new Error("Failed to login")
+    if (!response) throw new Error('Failed to login');
 
     const browserResult = await openAuthSessionAsync(
       response.toString(),
       redirectUri
-    )
+    );
 
-    if(browserResult.type !== "success") throw new Error("Failed to login");
+    if (browserResult.type !== 'success') throw new Error('Failed to login');
 
     const url = new URL(browserResult.url);
 
-    const secret = url.searchParams.get("secret")?.toString();
-    const userId = url.searchParams.get("userId")?.toString();
+    const secret = url.searchParams.get('secret')?.toString();
+    const userId = url.searchParams.get('userId')?.toString();
 
-    if(!secret || !userId) throw new Error("Failed to login");
+    if (!secret || !userId) throw new Error('Failed to login');
 
-    const session  = await account.createSession(userId, secret);
+    const session = await account.createSession(userId, secret);
 
-    if(!session) throw new Error("Failed to create a session");
+    if (!session) throw new Error('Failed to create a session');
 
     return true;
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return false;
   }
 }
 
+export async function Logout() {
+  try {
+    await account.deleteSession('current');
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
