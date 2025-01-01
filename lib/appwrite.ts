c940d5203436a4a74c2d49922a5365f2f0ac34bd
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-01 11:24:37 +0100

    add functionality to login

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,5 @@
-import { Account, Avatars, Client } from "react-native-appwrite";
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
 
 export const config = {
   platform: "com.jonahDev.realestate",
@@ -15,3 +16,37 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
+
+export async function Login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+
+    const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
+
+    if(!response) throw new Error("Failed to login")
+
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    )
+
+    if(browserResult.type !== "success") throw new Error("Failed to login");
+
+    const url = new URL(browserResult.url);
+
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+
+    if(!secret || !userId) throw new Error("Failed to login");
+
+    const session  = await account.createSession(userId, secret);
+
+    if(!session) throw new Error("Failed to create a session");
+
+    return true;
+  } catch (error) {
+    console.error(error)
+    return false;
+  }
+}
+
