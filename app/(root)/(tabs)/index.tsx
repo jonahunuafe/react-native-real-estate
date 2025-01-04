1790cdc54e236f8878786cea3397f0c272cedc6b
Jonah Unuafe <fejisfejiro@gmail.com>
2025-01-04 10:23:02 +0100

    implement featured card ui

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,15 @@
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Search from "@/components/Search";
+import { Card, FeaturedCard } from "@/components/Cards";
 
 export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       <View className="px-5">
-        <Search />
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
             <Image 
@@ -23,7 +23,21 @@ export default function Index() {
           </View>
           <Image source={icons.bell} className="size-6"/>
         </View>
+
+        <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <FeaturedCard />
+        <Card />
       </View>
+
     </SafeAreaView>
   );
 }
